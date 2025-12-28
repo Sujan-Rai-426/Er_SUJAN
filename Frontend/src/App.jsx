88731a168fb7dfa69544565f1810875598711234
Sujan <rsujan140.in@gmail.com>
2025-12-28 20:20:06 +0545

    Added Vercel analytics

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Navbar, Footer, } from './components/Components_Import'
 import Home from './pages/Home'
 import Interactive_Background from './utils/Interactive_Background'
 import Parent_API_Provider from './context/Parent_API_Provider'
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
 
@@ -25,6 +26,9 @@ function App() {
                 </Interactive_Background>
 
                     <Footer/>
+
+                    <Analytics />
+
                 </div>
             </BrowserRouter>
         </Parent_API_Provider>
